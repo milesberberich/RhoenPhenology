eb21098d6ecb66e303ac23aa7bfde9a36cf3c3c9
milesberberich <milesberberich@gmail.com>
2026-05-20 16:08:03 +0200

    Initial commit

---
+++ modis_loader.py
@@ -0,0 +1,40 @@
+import pystac_client
+import xarray as xr
+import geopandas as gpd
+import pystac_client
+import planetary_computer
+import odc.stac
+
+
+def modis_loader(aoi_path, index="NDVI", datetime="2025-01-01/2025-06-30"):
+    """Loads MODIS data to an xarray dataset using Microsoft Planetary Computer"""
+
+    # 1. Load aoi
+    aoi = gpd.read_file(aoi_path).to_crs("EPSG:4326")
+
+    # 2. Connect to Planetary Computer (with automatic token signing)
+    catalog = pystac_client.Client.open(
+        "https://planetarycomputer.microsoft.com/api/stac/v1",
+        modifier=planetary_computer.sign_inplace
+    )
+
+    # 3. Search the catalog
+    print("Searching catalog")
+    search = catalog.search(
+        collections=["modis-13Q1-061"],  # MPC uses a capital 'Q'
+        bbox=tuple(aoi.total_bounds),
+        datetime=datetime
+    )
+    items = search.item_collection()
+    print(f"Found {len(items)} items.")
+
+    # create dc
+    cube = odc.stac.load(
+        items,
+        assets=[f"250m_16_days_{index}"],
+        bbox=tuple(aoi.total_bounds),
+        crs="EPSG:4326",  # Forces standard Lat/Lon to match your AOI
+        resolution=0.00225  # ~250m converted to decimal degrees
+    )
+
+    return cube
