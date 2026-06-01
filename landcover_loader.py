14b09bb416cb7d3854faa2baeb8a4dc8ec8057d8
milesberberich <milesberberich@gmail.com>
2026-06-01 11:11:37 +0200

    fixed landcover loader, now it loads 2020 data, could be further improved to load exactly the year its used on to trim the MODIS data perfectly

--- landcover_loader.py
+++ landcover_loader.py
@@ -1,21 +1,18 @@
-import pystac_client
-import xarray as xr
 import geopandas as gpd
 import pystac_client
 import planetary_computer
 import odc.stac
 
-def landcover_loader(aoi_path, datetime="2025-01-01/2025-06-30"):
-    """Loads MODIS data to an xarray dataset using Microsoft Planetary Computer"""
+def landcover_loader(aoi_path, datetime="2020-01-01/2020-12-30"):
+    """Loads ESA-CCI-LC data to an xarray dataset using Microsoft Planetary Computer"""
 
     # 1. Load aoi
     aoi = gpd.read_file(aoi_path).to_crs("EPSG:4326")
 
-    # 2. Connect to Planetary Computer (with automatic token signing)
+    # 2. Connect to Planetary Computer
     catalog = pystac_client.Client.open(
         "https://planetarycomputer.microsoft.com/api/stac/v1",
-        modifier=planetary_computer.sign_inplace
-    )
+        modifier=planetary_computer.sign_inplace)
 
     # 3. Search the catalog
     print("Searching catalog")
@@ -34,13 +31,4 @@ def landcover_loader(aoi_path, datetime="2025-01-01/2025-06-30"):
         crs="EPSG:4326",
         resolution=0.00225
     )
-
-    # 5. NAN-handling, scaling and masking
-    qa_layer = cube["250m_16_days_VI_Quality"]
-    ndvicube = cube[f"250m_16_days_{index}"].where(cube[f"250m_16_days_{index}"] != -3000) * 0.0001
-    mask = (qa_layer & 3) <= 1
-    ndvicube = ndvicube.where(mask)
-
-    print(f"Found {len(items)} items ({ndvicube.nbytes / (1024 ** 2):.2F} MB")
-
-    return ndvicube
+    return cube
